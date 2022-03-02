8dfbf4a2ca608935d39e0b697b0f78e70aa898c3
alexcuat <alexvascuat@gmail.com>
2022-03-01 22:53:43 -0600

    se agrega el proyecto

---
+++ Adivinador de Numeros/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Adivinador_de_Numeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(201, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¿ ?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(466, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adivina el número generado entre el 1 y 50";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 73);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "¿ ?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(188, 222);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(117, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(228, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Introduce el número:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(163, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Adivinar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(487, 338);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Adivinador de Numeros/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adivinador_de_Numeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int contador_oportunidades = 0;
+        int oportunidades = 5;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            generadorNumero();
+        }
+        private void generadorNumero()
+        {
+            Random numero = new Random();
+            label3.Text = numero.Next(1, 100).ToString();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contador_oportunidades = contador_oportunidades + 1;
+            int opo = oportunidades - contador_oportunidades;
+            int a = Convert.ToInt32(label3.Text);
+            int b = Convert.ToInt32(textBox1.Text);
+            if (contador_oportunidades < 10)
+            {
+                //MessageBox.Show("todavia hay oportunidad");
+                if (a == b)
+                {
+                    MessageBox.Show("Correcto, adivinaste el número en tu intento número " + contador_oportunidades.ToString());
+                }
+                else {
+                    if (a > b)
+                    {
+                        MessageBox.Show("error, el número introducido es menor, no es correcto tienes " + opo.ToString() + " oportunidades más");
+                        textBox1.Clear();
+                        textBox1.Focus();
+                    }
+                    else {
+                        MessageBox.Show("error, el número introducido es mayor, no es correcto tienes " + opo.ToString() + " oportunidades más");
+                        textBox1.Clear();
+                        textBox1.Focus();
+                    }
+                }
+            }
+            else {
+                MessageBox.Show("lo siento se te acabaron las oportunidades, el número era "+label3.Text);
+            }
+
+        }
+    }
+}
